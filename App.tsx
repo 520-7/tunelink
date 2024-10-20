15cf723f5794911fedfb9ad506aca2d04f3a224d
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-10-20 12:49:09 -0400

    Google sign in

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './src/screens/SplashScreen';
@@ -9,10 +9,17 @@ import FeedScreen from './src/screens/FeedScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import MakePostScreen from './src/screens/MakePostScreen';
 import { RootStackParamList } from './src/navigation/RootStackParamList';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const App = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: '981133107700-rqi9fug0t7mkov2p2q7ivm6q2esgge0e.apps.googleusercontent.com',
+    });
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash">
